e07363d6c206e7043afc1e2daf25f454c79207f7
Adeyemi Glory <adeyemiglr@gmail.com>
2022-03-06 12:47:18 +0100

    Update Home.jsx

--- src/client/pages/home/Home.jsx
+++ src/client/pages/home/Home.jsx
@@ -17,6 +17,7 @@ function Home() {
     // getting all templates start
     const getTemp = async () => {
       const res = await axios.get(`/templates/${search}`);
+      console.log(res);
       setTemplates(res.data);
     };
     getTemp();
@@ -25,6 +26,7 @@ function Home() {
     // getting all cats start
     const getCats = async () => {
       const res = await axios.get("/categories");
+  console.log(res);
       setCats(res.data);
     };
     getCats();
